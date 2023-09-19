8080b258cf79732a9fbf36b064e8e832588d5a8a
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-18 21:24:28 -0500

    Update FacturaAndDetalle.java

--- TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/controladores/FacturaAndDetalle.java
+++ TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/controladores/FacturaAndDetalle.java
@@ -4,6 +4,7 @@ import com.example.transaccionessrpingbootmysql.modelo.DetalleFacturaModelo;
 import com.example.transaccionessrpingbootmysql.modelo.FacturaModelo;
 import com.example.transaccionessrpingbootmysql.servicio.FacturaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +31,25 @@ public class FacturaAndDetalle {
     public void guardarFacturaV3(@RequestBody FacturaModelo facturaModelo) {
         facturaService.guardarFacturaV3(facturaModelo);
     }
+    @PostMapping("/guardar/facturas/v4")
+    public void guardarFacturaV4(@RequestBody FacturaModelo facturaModelo) {
+        facturaService.guardarFacturaV4(facturaModelo);
+    }
+
+    @PostMapping("/guardar/facturas/v5")
+    public void guardarFacturaV5(@RequestBody FacturaModelo facturaModelo) throws MethodArgumentNotValidException {
+        facturaService.guardarFacturaV5(facturaModelo);
+    }
+
+    @PostMapping("/guardar/facturas/v6")
+    public void guardarFacturaV6( @RequestBody FacturaModelo facturaModelo) throws MethodArgumentNotValidException {
+        facturaService.guardarFacturaV7(facturaModelo);
+    }
+
+    @PostMapping("/guardar/facturas/v7")
+    public void guardarFacturaV7(@RequestBody FacturaModelo facturaModelo) throws MethodArgumentNotValidException {
+        facturaService.guardarFacturaV8(facturaModelo);
+    }
+
 
 }
