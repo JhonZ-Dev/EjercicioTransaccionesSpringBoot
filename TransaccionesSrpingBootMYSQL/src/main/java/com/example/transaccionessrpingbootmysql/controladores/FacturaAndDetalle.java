b6ce8f7e79446b3b1a00bf54029dbdb06dbc5dfa
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-18 21:23:26 -0500

    Update FacturaAndDetalle.java

--- TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/controladores/FacturaAndDetalle.java
+++ TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/controladores/FacturaAndDetalle.java
@@ -1,4 +1,12 @@
 package com.example.transaccionessrpingbootmysql.controladores;
 
+import com.example.transaccionessrpingbootmysql.servicio.FacturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FacturaAndDetalle {
+    @Autowired
+    public FacturaService facturaService;
+
 }
