6facdc81281b6e1fce3409048eef4ef2877fc378
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-18 21:23:43 -0500

    Update FacturaAndDetalle.java

--- TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/controladores/FacturaAndDetalle.java
+++ TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/controladores/FacturaAndDetalle.java
@@ -1,12 +1,24 @@
 package com.example.transaccionessrpingbootmysql.controladores;
 
+import com.example.transaccionessrpingbootmysql.modelo.DetalleFacturaModelo;
+import com.example.transaccionessrpingbootmysql.modelo.FacturaModelo;
 import com.example.transaccionessrpingbootmysql.servicio.FacturaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FacturaAndDetalle {
     @Autowired
     public FacturaService facturaService;
+
+    @PostMapping("/guardarFactura")
+    public void guardarFactura(@RequestBody FacturaModelo facturaModelo) {
+        List<DetalleFacturaModelo> detalles = facturaModelo.getFacturaModelos();
+        facturaService.guardarFactura(facturaModelo, detalles);
+    }
 
 }
