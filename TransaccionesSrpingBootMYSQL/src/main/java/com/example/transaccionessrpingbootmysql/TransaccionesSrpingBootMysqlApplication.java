b5f154e97617d0081d1fd64fd7901c8ccf251348
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-18 21:09:04 -0500

    Estructura del proyecto

---
+++ TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/TransaccionesSrpingBootMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.transaccionessrpingbootmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransaccionesSrpingBootMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransaccionesSrpingBootMysqlApplication.class, args);
+	}
+
+}
