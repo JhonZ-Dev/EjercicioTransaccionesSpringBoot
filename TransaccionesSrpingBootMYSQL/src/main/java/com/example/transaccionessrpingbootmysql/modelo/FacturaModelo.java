f34b620c60c58e509f9abc3c851d82fe4ce316fa
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-18 21:13:24 -0500

    Create FacturaModelo.java

---
+++ TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/modelo/FacturaModelo.java
@@ -0,0 +1,18 @@
+package com.example.transaccionessrpingbootmysql.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Data
+@Table(name = "tbl_factura")
+public class FacturaModelo {
+    
+}
