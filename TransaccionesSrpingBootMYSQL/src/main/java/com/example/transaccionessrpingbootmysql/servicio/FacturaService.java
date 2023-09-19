e3b796a7302c71774e62c5602a717e68f33c86af
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-18 21:19:47 -0500

    Update FacturaService.java

--- TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/servicio/FacturaService.java
+++ TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/servicio/FacturaService.java
@@ -1,7 +1,11 @@
 package com.example.transaccionessrpingbootmysql.servicio;
 
+import com.example.transaccionessrpingbootmysql.repositorio.DetalleFacRepo;
+import com.example.transaccionessrpingbootmysql.repositorio.FacturaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FacturaService {
 
     @Autowired
