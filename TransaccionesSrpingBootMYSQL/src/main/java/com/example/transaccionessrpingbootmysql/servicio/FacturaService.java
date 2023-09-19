18a34ab90de7468140d5a31ba579cc9d11ad7680
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-18 21:20:10 -0500

    Update FacturaService.java

--- TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/servicio/FacturaService.java
+++ TransaccionesSrpingBootMYSQL/src/main/java/com/example/transaccionessrpingbootmysql/servicio/FacturaService.java
@@ -1,9 +1,14 @@
 package com.example.transaccionessrpingbootmysql.servicio;
 
+import com.example.transaccionessrpingbootmysql.modelo.DetalleFacturaModelo;
+import com.example.transaccionessrpingbootmysql.modelo.FacturaModelo;
 import com.example.transaccionessrpingbootmysql.repositorio.DetalleFacRepo;
 import com.example.transaccionessrpingbootmysql.repositorio.FacturaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class FacturaService {
@@ -12,4 +17,16 @@ public class FacturaService {
     public FacturaRepo facturaRepo;
     @Autowired
     public DetalleFacRepo detalleFacRepo;
+
+    @Transactional
+    public void guardarFactura(FacturaModelo facturaModelo, List<DetalleFacturaModelo> detalles) {
+        facturaModelo.setFacturaModelos(detalles); // Establecer la relación entre factura y detalles
+        facturaRepo.save(facturaModelo); // Guardar la factura (esto también guardará los detalles debido a la cascada definida en la relación)
+
+        // Asegurarse de establecer la relación entre cada detalle y la factura antes de guardar los detalles
+        for (DetalleFacturaModelo detalle : detalles) {
+            detalle.setFacturaModelo(facturaModelo);
+            detalleFacRepo.save(detalle); // Guardar cada detalle
+        }
+    }
 }
